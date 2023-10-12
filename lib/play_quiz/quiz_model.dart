7eb99b991fb73415d23fae55f0d23305f35ca260
rapunschel <rapunschel.gaming@gmail.com>
2023-10-12 20:20:29 +0200

    Implemented provider for quiz page

---
+++ lib/play_quiz/quiz_model.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class QuizModel extends ChangeNotifier {
+  Quiz? quiz = initiateQuiz();
+
+  // Index for current question
+  int currentQuestionIndex = 0;
+
+  // Counters for guesses
+  int noCorrect = 0;
+  int noIncorrect = 0;
+
+  QuizModel();
+
+  int getNumberOfQuestions() {
+    return quiz!.questions.length;
+  }
+
+  List getCurrentAnswers() {
+    return quiz!.questions[currentQuestionIndex].answers;
+  }
+
+  Question getCurrentQuestion() {
+    return quiz!.questions[currentQuestionIndex];
+  }
+
+  void getNextQuestion() {
+    // Only increment if we are in range.
+    if (currentQuestionIndex < quiz!.questions.length - 1) {
+      currentQuestionIndex++;
+      notifyListeners();
+    }
+  }
+}
+
+// All below for test run only, delete later
+Quiz initiateQuiz() {
+  Quiz quiz = Quiz("Capital of countries");
+
+  Question question1 = Question("Capital of Sweden?");
+  question1.addAnswer("Malmö", false);
+  question1.addAnswer("Gothenburg", false);
+  question1.addAnswer("Stockholm", true);
+
+  Question question2 = Question("Capital of England?");
+  question2.addAnswer("London", true);
+  question2.addAnswer("Berlin", false);
+  question2.addAnswer("Brighton", false);
+  question2.addAnswer("Edinburgh", false);
+
+  Question question3 = Question("Capital of Japan?");
+  question3.addAnswer("Tokyo", true);
+  question3.addAnswer("Kyoto", false);
+  question3.addAnswer("Osaka", false);
+  question3.addAnswer("Fukouka", false);
+
+  quiz.addQuestion(question1);
+  quiz.addQuestion(question2);
+  quiz.addQuestion(question3);
+
+  return quiz;
+}
+
+// Classes to model a quiz
+class Quiz {
+  String title;
+  List questions = [];
+
+  Quiz(this.title);
+
+  void addQuestion(Question question) {
+    questions.add(question);
+  }
+}
+
+class Question {
+  String title;
+  List answers = [];
+
+  Question(this.title);
+
+  void addAnswer(String text, bool isCorrect) {
+    answers.add(Answer(text, isCorrect));
+  }
+}
+
+class Answer {
+  String text;
+  bool isCorrect;
+
+  Answer(this.text, this.isCorrect);
+}
---
+++ lib/play_quiz/test_play_quiz.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'quiz_model.dart';
+
+void main() {
+  runApp(const Test());
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => QuizModel(),
+      child: MaterialApp(
+        title: 'quiz run test',
+        theme: ThemeData(
+          primaryColor: Colors.green,
+          primaryColorLight: Colors.green,
+          useMaterial3: true,
+        ),
+        home: const PlayQuizPage(),
+      ),
+    );
+  }
+}
+
+class PlayQuizPage extends StatelessWidget {
+  const PlayQuizPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Listen to the currentIndex.
+    context.watch<QuizModel>().currentQuestionIndex;
+
+    // Retrieve the QuizModel
+    QuizModel quiz = context.read<QuizModel>();
+
+    // Get current question & the answers
+    Question question = quiz.getCurrentQuestion();
+    List answers = quiz.getCurrentAnswers();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        //  title: Center(child: Text(quiz.title)),
+      ),
+      body: Center(
+        child: ListView.builder(
+            itemCount: answers.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                // If index 0, put the question
+                children: index == 0
+                    ? <Widget>[
+                        // Question title
+                        Text(question.title),
+                        // Put space between
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 150),
+                        ),
+                        AnswerTileWidget(answer: answers[index]),
+                      ]
+                    : <Widget>[
+                        const Padding(padding: EdgeInsets.only(top: 25)),
+                        AnswerTileWidget(
+                          answer: answers[index],
+                        ),
+                      ],
+              );
+            }),
+      ),
+    );
+  }
+}
+
+class AnswerTileWidget extends StatelessWidget {
+  const AnswerTileWidget({
+    super.key,
+    required this.answer,
+  });
+  final Answer answer;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Center(child: Text(answer.text)),
+
+      onTap: () {
+        // TODO Listen if an answer been pressed,
+        // update listTile color based on correct / wrong
+        if (answer.isCorrect) {
+          context.read<QuizModel>().getNextQuestion();
+        }
+      },
+
+      // Styling
+      tileColor: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+    );
+  }
+}
