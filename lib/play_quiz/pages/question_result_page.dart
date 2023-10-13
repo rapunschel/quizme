0873ab7c30d517d4b221b51abe14cdbf124d54b4
rapunschel <rapunschel.gaming@gmail.com>
2023-10-13 21:14:30 +0200

    Added view to see what user answered & the correct answers to question

---
+++ lib/play_quiz/pages/question_result_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../providers/quiz_model.dart';
+
+class QuestionResultPage extends StatelessWidget {
+  const QuestionResultPage({super.key, required this.question});
+
+  final (Question, Answer) question;
+
+  @override
+  Widget build(BuildContext context) {
+    //QuizModel quiz = context.read<QuizModel>();
+    List answers = question.$1.answers;
+    List correctAnswers = answers.where((answer) => answer.isCorrect).toList();
+    int noCorrectAnswers = correctAnswers.length;
+
+    // Initialize list with user's answer
+    List<Widget> buildList = [];
+    buildList.add(Text(question.$1.title));
+    buildList.add(const Text("You answered"));
+    buildList.add(
+      AnswerCardTile(answer: question.$2),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(question.$1.title),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: noCorrectAnswers,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            buildList.addAll(
+              [
+                const Text("Correct answers"),
+                AnswerCardTile(answer: correctAnswers[index])
+              ],
+            );
+            return Column(
+              children: buildList,
+            );
+          }
+          // else return the rest of correct answers
+          if (index < noCorrectAnswers) {
+            return AnswerCardTile(answer: correctAnswers[index]);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class AnswerCardTile extends StatelessWidget {
+  const AnswerCardTile({
+    super.key,
+    required this.answer,
+  });
+
+  final Answer answer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.only(top: 12.5, bottom: 12.5, left: 25, right: 25),
+      child: ListTile(
+        title: Text(
+          answer.text,
+          textAlign: TextAlign.center,
+        ),
+        tileColor: Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+    );
+  }
+}
