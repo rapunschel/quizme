570c9063f25621701b01c02718673dcccb59479d
rapunschel <rapunschel.gaming@gmail.com>
2023-10-14 11:12:58 +0200

    added some textstyling to TextTheme & google font

--- lib/play_quiz/test_play_quiz.dart
+++ lib/play_quiz/test_play_quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/quiz_model.dart';
 import 'pages/play_quiz_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Play());
@@ -17,10 +18,29 @@ class Play extends StatelessWidget {
       child: MaterialApp(
         title: 'quiz run test',
         theme: ThemeData(
-          primaryColor: const Color.fromARGB(255, 192, 241, 251),
-          primaryColorLight: Colors.green,
-          useMaterial3: true,
-        ),
+            primaryColor: const Color.fromARGB(255, 192, 241, 251),
+            primaryColorLight: Colors.green,
+            fontFamily: GoogleFonts.openSans().fontFamily,
+            useMaterial3: true,
+            appBarTheme: AppBarTheme(
+              titleTextStyle: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontWeight: FontWeight.normal),
+            ),
+            textTheme: const TextTheme(
+                // Global styling, use Theme... to use a specific style
+                //and copyWith to overwrite specific values
+                titleLarge: TextStyle(fontWeight: FontWeight.bold),
+                // Edit
+                titleMedium:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                titleSmall: TextStyle(),
+                bodyLarge: TextStyle(),
+                bodyMedium: TextStyle(),
+                // Buttons uses labelLarge
+                labelLarge:
+                    TextStyle(fontSize: 15, fontWeight: FontWeight.bold))),
         home: const PlayQuizPage(),
       ),
     );
