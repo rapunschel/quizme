bcffb455e4441e013416e05ef45b007cdbf2e796
rapunschel <rapunschel.gaming@gmail.com>
2023-10-18 20:08:55 +0200

    Added a handler class using provider for retrieving all quizzes made

---
+++ lib/providers/quizzes_handler.dart
@@ -0,0 +1,21 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'quiz_model.dart';
+
+// Contain a list of all quizzes made.
+class QuizHandler extends ChangeNotifier {
+  List<Quiz> _quizzes = [initiateQuiz(), initiateQuiz2()];
+
+  void addQuiz(Quiz quiz) {
+    _quizzes.add(quiz);
+  }
+
+  void removeQuiz(Quiz quiz) {
+    throw UnimplementedError();
+  }
+
+  List<Quiz> getQuizzes() {
+    return _quizzes;
+  }
+}
